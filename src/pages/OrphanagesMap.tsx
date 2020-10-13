3b2e0364d46b4d6794cb37853a9de56eaf45701e
plz09 <62223358+plz09@users.noreply.github.com>
2020-10-13 08:38:05 +0100

    Inserting the map into orphanage-map page
    Inserting the map into orphanage-map page with "open street map"

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
@@ -22,7 +25,13 @@ function OrphanagesMap() {
                 </footer>
             </aside>
 
-            <div></div>
+            <Map 
+                center={[38.7437396,-9.2302437]}
+                zoom={12}
+                style={{ width: '100%', height: '100%' }}
+            >
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            </Map>
 
             <Link to="" className="create-orphanage">
                 <FiPlus size={32} color="#FFF" />
