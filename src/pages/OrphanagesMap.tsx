ebdd714e36c9fdd2908fc39e7c264ce55f9cf03e
plz09 <62223358+plz09@users.noreply.github.com>
2020-10-16 20:51:41 +0100

    Changing map to mapbox

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -39,8 +39,12 @@ function OrphanagesMap() {
                 zoom={12}
                 style={{ width: '100%', height: '100%' }}
             >
-                <TileLayer 
+                {/* <TileLayer 
                     url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" 
+                /> */} 
+
+                <TileLayer 
+                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                 />
 
                 <Marker
